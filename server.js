7281cb9c49dfb51e0506d77bd416592e22819be3
zamboilles21 <zamboilles@turr.hu>
2022-09-22 09:56:39 +0200

    5

--- server.js
+++ server.js
@@ -1,6 +1,27 @@
-const express = require("express")
+const express = require("express");
+const req = require("express/lib/request");
+const res = require("express/lib/response");
 const app=express();
 const port=5000;
+var mysql=require('mysql');
+
+var pool  = mysql.createPool({
+    connectionLimit : 10,
+    host            : 'localhost',
+    user            : 'root',
+    password        : '',
+    database        : '2/14szft_elso'
+  });
+
+app.get('/laptopok',(req,res)=>{
+    pool.query(`SELECT * FROM laptopok`,(err,result)=>{
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(result);
+        }
+    });
+});
 
 app.get('/',(req,res)=>{
     res.status(200).send('Gyakorlás');
