f980bc649f14851d48a1cfdaf5f589d9cb38b333
zamboilles21 <zamboilles@turr.hu>
2022-09-23 09:29:28 +0200

    fel

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
+require('dotenv').config()
 const express = require("express");
 const req = require("express/lib/request");
 const { type } = require("express/lib/response");
 const res = require("express/lib/response");
 const app=express();
-const port=5000;
+const port=process.env.PORT;
 var mysql=require('mysql');
 
 var pool  = mysql.createPool({
     connectionLimit : 10,
-    host            : 'localhost',
-    user            : 'root',
-    password        : '',
-    database        : '2/14szft_elso'
+    host            : process.env.DBHOST,
+    user            : process.env.DBUSER,
+    password        : process.env.DBPASS,
+    database        : process.env.DBNAME
   });
 
 app.use(express.urlencoded({extended:true}));
@@ -37,23 +38,58 @@ app.get('/laptopok/:id',(req,res)=>{
     });
 });
 
-app.post('/ujlaptop', (req, res)=>{
-    let adatok={
-        "brand":req.body.brandname,
-        "type":req.body.type,
-        "des":req.body.des,
-        "price":req.body.price
-
-    }
-    pool.query(`INSERT INTO laptopok VALUES(null,'${adatok.brand}',${adatok.type}',${adatok.des}',${adatok.price}')`,(err,result)=>{
+app.post('/laptoptorles/:id', (req, res) => {
+    let laptopID = req.params.id;
+    pool.query(`DELETE FROM laptopok WHERE ID=${laptopID}`, (err, results) => {
         if (err) {
             res.status(500).send(err);
         } else {
-            res.status(200).send(result);
+            res.status(200).send(results);
         }
-    })
+    });
 });
 
+app.get('/osszeslaptoptorles', (req, res) => {
+    pool.query(`DELETE FROM laptopok`, (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(results);
+        }
+    });
+});
+
+app.post('/ujlaptop', (req, res) => {
+    let adatok = {
+        "brand": req.body.brandname,
+        "type": req.body.type,
+        "des": req.body.des,
+        "price": req.body.price
+    }
+    pool.query(`INSERT INTO laptopok VALUES(null, '${adatok.brand}', '${adatok.type}', '${adatok.des}', ${adatok.price})`, (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(results);
+        }
+    });
+});
+app.post('/laptopmod/:id', (req, res) => {
+    let laptopID = req.params.id;
+    let adatok = {
+        "brand": req.body.brandname,
+        "type": req.body.type,
+        "des": req.body.des,
+        "price": req.body.price
+    }
+    pool.query(`UPDATE laptopok SET brand='${adatok.brand}', type='${adatok.type}', des='${adatok.des}', price=${adatok.price} WHERE ID=${laptopID}`, (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(results);
+        }
+    });
+});
 app.get('/',(req,res)=>{
     res.status(200).send('Gyakorlás');
 });
